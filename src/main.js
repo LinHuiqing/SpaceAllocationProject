db783ab96e1c328b7f474703c4ad349180719cf0
LinHuiqing <lin.hui.qing1999@gmail.com>
2020-03-03 01:09:14 +0800

    changed drag and drop package, and formatted page

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
+import VueDraggableResizable from 'vue-draggable-resizable'
+// optionally import default styles
+import 'vue-draggable-resizable/dist/VueDraggableResizable.css'
 
+Vue.component('vue-draggable-resizable', VueDraggableResizable)
 Vue.use(Buefy)
 
 Vue.config.productionTip = false

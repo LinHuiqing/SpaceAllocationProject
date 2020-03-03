5f0c2a2c81bcc125b7935d741abfc6baebbf6002
alicezhangjy <41232260+alicezhangjy@users.noreply.github.com>
2020-03-03 18:08:32 +0800

    map for capstone lvl 1

--- src/App.vue
+++ src/App.vue
@@ -14,8 +14,8 @@
       </section>
       <nav class="navbar is-light" role="navigation" aria-label="main navigation">
         <div class="navbar-brand">
-          <a class="navbar-item" href="https://bulma.io">
-            <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: Free, open source, and modern CSS framework based on Flexbox" width="112" height="28">
+          <a class="navbar-item" href="https://github.com/LinHuiqing/SpaceAllocationProject">
+            <img src="./assets/logo_transparent2.png" alt="Bulma: Free, open source, and modern CSS framework based on Flexbox" width="80" height="100">
           </a>
           <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
             <span aria-hidden="false"></span>
@@ -43,7 +43,7 @@
                   <p class="subtitle">Campus Centre Level 1</p>
                   <div>
                     <figure class="image">
-                      <img src="https://bulma.io/images/placeholders/128x128.png">
+                      <img src="./assets/capstone1.jpg">
                     </figure>
                   </div>
                 </article>

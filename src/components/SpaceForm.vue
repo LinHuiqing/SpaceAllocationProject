bac0622a9870dd7b6a4104e68fd5d256dd9551bc
alicezhangjy <jingyu_zhang@mymail.sutd.edu.sg>
2020-03-25 19:44:13 +0800

    added Space form into current branch

---
+++ src/components/SpaceForm.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="tile is-parent is-vertical">
+        <p class="title">Request for space</p>
+        <p class="subtitle">Use this form to request for space.</p>
+
+        <SizeSelectionRow></SizeSelectionRow>
+
+        <PowerAVRow></PowerAVRow>
+    </div>
+</template>
+
+<script>
+import SizeSelectionRow from './space_form/SizeSelectionRow';
+import PowerAVRow from './space_form/PowerAVRow.vue';
+
+export default {
+  name: "SpaceForm",
+  components: {
+    SizeSelectionRow, PowerAVRow
+  }
+}
+</script>
---
+++ src/components/space_form/IncrementDecrementCard.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="tile is-parent is-4 increment-decrement-card">
+        <div class="card">
+            <div class="card-content">
+                <div class="tile is-parent is-vertical">
+                    <div class="tile is-child title">
+                        <p class="title">{{ title }}</p>
+                    </div>
+                    <div class="tile is-child description">
+                        <p>{{ description }}</p>
+                    </div>
+                    <div class="tile is-child">
+                        <section>
+                            <b-field label="Quantity">
+                                <b-numberinput
+                                    min="0"
+                                    v-model="internal_value"
+                                    v-on:input="valueChanged">
+                                </b-numberinput>
+                            </b-field>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "increment-decrement-card",
+    data: function(){ 
+        return {
+            "internal_value": this.value ?? 0, 
+        };
+    },
+    props: {
+        "title": String,
+        "description": String,
+        "value": Number,
+    },
+    methods: {
+        valueChanged: function(value){
+            this.$emit("valueChanged", {
+                "title": this.title,
+                "value": value,
+            });
+        },
+    }
+}
+</script>
+
+<style scoped>
+/*
+This is to ensure the card contents are all of the same height so that the input buttons are all aligned
+ */
+.increment-decrement-card .card-content {
+    height: 100%;
+}
+
+.increment-decrement-card .card-content .is-parent{
+    height: 100%;
+}
+
+.increment-decrement-card .card {
+    width: 100%;
+}
+</style>

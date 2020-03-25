61de2e65b86dcf5d2775406803a7afc1ef518cf6
alicezhangjy <jingyu_zhang@mymail.sutd.edu.sg>
2020-03-25 23:21:16 +0800

    nav bar fixed

--- src/components/navigation.vue
+++ src/components/navigation.vue
@@ -9,21 +9,8 @@
             </b-navbar-item>
         </template>
         <template slot="start">
-            <!-- <b-navbar-item href="#">
-                Home
-            </b-navbar-item>
-            <b-navbar-item router-link to="/level1">trial</b-navbar-item>
-            <b-navbar-dropdown label="Map">
-                <b-navbar-item router-link to="/level2">Level 1</b-navbar-item>
-                <b-navbar-item href="#">
-                    Level 1
-                </b-navbar-item>
-                <b-navbar-item tag="router-link" :to="{ name: 'level2' }">
-                    Level 2
-                </b-navbar-item>
-            </b-navbar-dropdown> -->
-            <router-link to="/level1">Level 1</router-link>
-            <router-link to="/level2">Level 2</router-link>
+            <router-link to="/level1" class="navbar-item">Level 1</router-link>
+            <router-link to="/level2" class="navbar-item">Level 2</router-link>
         </template>
 
         <template slot="end">

41abbe9b85fa9679d5651c9f4eadeac146723463
chesnutcase <chester8991@gmail.com>
2020-03-28 11:35:24 +0800

    update form

--- src/components/space_form/TablePedestalRow.vue
+++ src/components/space_form/TablePedestalRow.vue
@@ -2,37 +2,38 @@
     <div>
         <p class="title">Tables &amp; Pedestals</p>
         <p class="subtitle">Change Quantity As Appropiate</p>
-        <div class="tile is-ancestor">
-            <IncrementDecrementCard
-                title="Small Tables"
-                description="0.5x0.5 Table"
-                v-bind:value="this.values['Small Tables']"
-                v-on:valueChanged="valueChanged">
-            </IncrementDecrementCard>
+        <div class="tile is-ancestor is-12 is-vertical">
+            <div class="tile is-12">
+                <IncrementDecrementCard
+                    title="Small Tables"
+                    description="0.5x0.5 Table"
+                    v-bind:value="this.values['Small Tables']"
+                    v-on:valueChanged="valueChanged">
+                </IncrementDecrementCard>
 
 
-            <IncrementDecrementCard
-                title="Large Tables"
-                description="1x1 Table"
-                v-bind:value="this.values['Large Tables']"
-                v-on:valueChanged="valueChanged">
-            </IncrementDecrementCard>
+                <IncrementDecrementCard
+                    title="Large Tables"
+                    description="1x1 Table"
+                    v-bind:value="this.values['Large Tables']"
+                    v-on:valueChanged="valueChanged">
+                </IncrementDecrementCard>
 
-            <IncrementDecrementCard
-                title="Small Pedestal"
-                description="0.5x0.5x1 pedestal to prominently display a prototype."
-                v-bind:value="this.values['Small Pedestals']"
-                v-on:valueChanged="valueChanged">
-            </IncrementDecrementCard>
-
-
-            <IncrementDecrementCard
-                title="Large Pedestal"
-                description="1x1x0.5 pedestal to prominently display a prototype."
-                v-bind:value="this.values['Large Pedestals']"
-                v-on:valueChanged="valueChanged">
-            </IncrementDecrementCard>
-
+                <IncrementDecrementCard
+                    title="Small Pedestal"
+                    description="0.5x0.5x1 pedestal to prominently display a prototype."
+                    v-bind:value="this.values['Small Pedestals']"
+                    v-on:valueChanged="valueChanged">
+                </IncrementDecrementCard>
+            </div>
+            <div class="tile is-12">
+                <IncrementDecrementCard
+                    title="Large Pedestal"
+                    description="1x1x0.5 pedestal to prominently display a prototype."
+                    v-bind:value="this.values['Large Pedestals']"
+                    v-on:valueChanged="valueChanged">
+                </IncrementDecrementCard>
+            </div>
         </div>
     </div>
 </template>

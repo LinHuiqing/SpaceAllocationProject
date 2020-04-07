c758c1100e0ef495614c9d8de02ec597562fb17f
alicezhangjy <jingyu_zhang@mymail.sutd.edu.sg>
2020-04-07 19:43:14 +0800

    no more head nav bar

--- src/App.vue
+++ src/App.vue
@@ -16,18 +16,6 @@
           </div>
         </div>
       </section>
-      <nav class="navbar is-light" role="navigation" aria-label="main navigation">
-        <div class="navbar-brand">
-          <a class="navbar-item" href="https://github.com/LinHuiqing/SpaceAllocationProject">
-            <img src="./assets/logo_transparent2.png" alt="Bulma: Free, open source, and modern CSS framework based on Flexbox" width="80" height="100">
-          </a>
-          <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
-            <span aria-hidden="false"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </a>
-        </div>
-      </nav>
       <!-- <router-link to="/level1">Level 1</router-link>
       <router-link to="/level2">Level 2</router-link> -->
       <app-nav></app-nav>

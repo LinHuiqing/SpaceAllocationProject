e1b8f6f294d1cd1419141f2f34b2865baa2a7fb9
alicezhangjy <jingyu_zhang@mymail.sutd.edu.sg>
2020-04-07 23:52:55 +0800

    resized and edited the layout

--- src/components/SpaceForm.vue
+++ src/components/SpaceForm.vue
@@ -1,29 +1,41 @@
 <template>
-    <div class="tile is-parent is-vertical">
-        <p class="title">Request for space</p>
-        <p class="subtitle">Use this form to request for space.</p>
+    <div class="tile is-parent is-vertical" :class="$style.form">
+      <div class="tile is-parent is-vertical">
+        <h1 class="title">Request for space</h1>
+        <h1 class="subtitle">Use this form to request for space.</h1>
+      </div>
 
         <div class="row">
           <form @submit.prevent="submitGroup" class="col s12">
-            <div class="row">
-              <div class="input-field col s12">
-                <b-field label="Capstone Group Number">
-                  <b-input type="number" v-model="group_no" required></b-input>
+            <div class="tile is-parent">
+              <div class="input-field">
+                <b-field label="Capstone Group Number" type="is-info" >
+                  <b-input type="number" v-model="group_no" placeholder="Enter Group number" required></b-input>
                 </b-field>
               </div>
             </div>
-            <SizeSelectionRow></SizeSelectionRow>
-
-           <PowerAVRow></PowerAVRow>
-
-           <b-field label="Theme">
-             <b-input v-model="theme" required></b-input>
+            <div class="tile is-parent">
+            <div class="input-field">
+          <b-field label="Theme" type="is-info">
+             <b-input type="text" v-model="theme" placeholder="Enter project theme" required></b-input>
            </b-field>
+            </div>
+            </div>
+            
+            <div class="tile is-parent" name="sizerow">
+            <SizeSelectionRow></SizeSelectionRow>
+            </div>
+            <div class="tile is-parent">
+           <PowerAVRow></PowerAVRow>
+           </div>
+          
 
+           <div class="tile is-parent">
            <div class="buttons">
             <b-button type="is-success" @click="submitGroup">Submit</b-button>
             <b-button type="is-danger"><router-link to="/" style="all: inherit;">Cancel</router-link></b-button>
           </div>
+           </div>
           </form>
         </div>
 
@@ -65,3 +77,9 @@ export default {
 }
 }
 </script>
+
+<style module>
+.form {
+    transform: scale(0.95);
+}
+</style>
